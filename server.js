6707d0b83a3909664a75a7b45dfd5b03c551eaed
Demotive <mat@demotive.com>
2016-03-23 17:36:00 +0000

    Simple build out of front end

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Glance test' });
+});
+
+app.get('/test', function (req, res) {
+  res.render('test', { title: 'Glance test' });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
