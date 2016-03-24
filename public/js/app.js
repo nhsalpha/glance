b71d2d975dee6c1dd66963b5483c381cf9b00cac
Dominic Baggott <dominic.baggott@gmail.com>
2016-03-24 13:53:29 +0000

    Rough runTrial implementation

--- public/js/app.js
+++ public/js/app.js
@@ -2,30 +2,49 @@ var init = function() {
   //set up stage
 }
 
-var showFixationRectangle = function() {
+container = document.getElementById("container");
+maskElement = document.getElementById("mask");
+wordElement = document.getElementById("word");
 
+var timeoutPromise = function(duration) {
+  return new Promise(function(fulfill, reject) {
+    window.setTimeout(fulfill, duration);
+  });
+};
+
+var showFixationRectangle = function() {
+  container.className = "fixation-rectangle";
+
+  return timeoutPromise(1000);
 };
 
 var showMask = function() {
-  document.getElementById('mask')
+  container.className = "mask";
+
+  return timeoutPromise(200);
 };
 
-var showWord = function() {
-  
+var showWord = function(word, exposureDuration) {
+  wordElement.textContent = word;
+  container.className = "word";
+
+  return timeoutPromise(exposureDuration);
 };
 
 var awaitResponse = function() {
+  container.className = "prompt";
 
+  return timeoutPromise(5000);
 };
 
-var runTrial = function(word) {
+var runTrial = function(word, exposureDuration) {
   return showFixationRectangle()
     .then(showMask)
-    .then(function() { return showWord(word, exposureDuration)); })
+    .then(function() { return showWord(word, exposureDuration); })
     .then(showMask)
-    .then(awaitResponse)
+    .then(awaitResponse);
 }
 
-runTrial("fart");
+runTrial("fart", 1000);
 
 //document.getElementById('word').innerHTML(word);
