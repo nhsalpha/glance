765101e00af1c7a017f47349905908e4c7b30b19
Dominic Baggott <dominic.baggott@gmail.com>
2016-03-29 11:16:06 +0100

    Return full result log from runSeries promise

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,6 @@ var showFixationRectangle = function() {
 };
 
 var showMask = function() {
-
   var mask = '';
   for (var i=0; i<= wordLength; i++) {
     mask += nonLetterCharacters[Math.floor(Math.random() * nonLetterCharacters.length)];
@@ -44,16 +43,14 @@ var awaitResponse = function(real) {
 
   var listener;
   var promise = new Promise(function(fulfill, reject) {
-    window.setTimeout(reject, 5000);
+    window.setTimeout(function() { reject("timeout"); }, 5000);
 
     listener = function(event) {
-      console.log(event);
-
-      if (event.code === "KeyQ") {
-        real ? fulfill() : reject();
+      if (event.keyCode === 81) {
+        real ? fulfill("real") : reject("real");
       }
-      else if (event.code === "KeyP") {
-        real ? reject() : fulfill();
+      else if (event.keyCode === 80) {
+        real ? reject("pseudo") : fulfill("pseudo");
       }
     };
 
@@ -78,22 +75,36 @@ var runTrial = function(word, exposureDuration) {
 
 var runSeries = function(words) {
   var words = words.slice(0),
+      log = [],
       exposureDuration = 1000,
       x = function x() {
         if (words.length > 0) {
-          return runTrial(words.shift(), exposureDuration).then(
-            function() {
+          var nextWord = words.shift(),
+              trialResult = {
+                word: nextWord.text,
+                real: nextWord.real,
+                duration: exposureDuration
+              };
+
+          return runTrial(nextWord, exposureDuration).then(
+            function(response) {
+              trialResult.response = response;
+              log.push(trialResult);
+
               exposureDuration = exposureDuration * 0.75;
               return x();
             },
-            function() {
+            function(response) {
+              trialResult.response = response;
+              log.push(trialResult);
+
               exposureDuration = exposureDuration * 1.5;
               return x();
             }
           );
         }
         else {
-          return "DONE";
+          return log;
         }
       };
 
@@ -108,4 +119,6 @@ var wordList = [
   {text: "flufed", real: false}
 ];
 
-runSeries(wordList).then(window.alert);
+runSeries(wordList).then(function(log) {
+  console.log(log);
+});
