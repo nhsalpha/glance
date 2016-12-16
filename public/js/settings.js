8bf3154f25a436c7a47641d40fa447b2ade36aeb
Demotive <mat@demotive.com>
2016-12-16 10:16:43 +0000

    Add 'system' font capability

--- public/js/settings.js
+++ public/js/settings.js
@@ -2,8 +2,8 @@ container = document.getElementById("container");
 maskElement = document.getElementById("mask");
 wordElement = document.getElementById("word");
 
-fonts = ["fs-me", "frutiger"];
-polarity = ["polarity-normal", "polarity-reversed"];
+fonts = ["system", "frutiger"];
+polarity = ["polarity-normal"];
 wordLength = 6;
 
 maxInterval = 1000;
